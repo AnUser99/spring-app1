ecea0c97235691f457077681d4345cbecd8e3792
AnUser99 <kisamasyanya@gmail.com>
2022-12-22 14:47:57 +0300

    Lesson -8

--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -8,19 +8,22 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
 //        TestBean testBean = context.getBean("testBean", TestBean.class);
 //        System.out.println(testBean.getName());
 
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 //        musicPlayer.playMusic();
-        System.out.println(musicPlayer1 == musicPlayer2);
-        musicPlayer1.setVolume(20);
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+//        System.out.println(musicPlayer1 == musicPlayer2);
+//        musicPlayer1.setVolume(20);
+//        System.out.println(musicPlayer1.getVolume());
+//        System.out.println(musicPlayer2.getVolume());
 
         context.close();
     }
