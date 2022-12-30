33e9d8e6633a90356799b9c501a9032e31ba6a57
AnUser99 <kisamasyanya@gmail.com>
2022-12-30 03:10:59 +0300

    Commit_test

--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -10,11 +10,15 @@ public class TestSpring {
 
         Music music = context.getBean("rockMusic", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.setMusic(music);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
 
         Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+        MusicPlayer classicalMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        classicalMusicPlayer.setMusic(music2);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
         classicalMusicPlayer.playMusic();
 
         context.close();
